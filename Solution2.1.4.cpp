d0933270bb4a415ba8fb075bc1422d6c3cbca139
sundingh <sundingh@gmail.com>
2016-12-17 13:20:23 +0800

    LeetCode2.1.4

--- Solution2.1.4.cpp
+++ Solution2.1.4.cpp
@@ -9,7 +9,7 @@ Write a function to determine if a given target is in the array.
 using namespace  std;
 class Solution {
 	public:
-		bool search(const vector<int>& nums, int target) {
+		int search(const vector<int>& nums, int target) {
 			int first = 0, last = nums.size();
 			while (first != last) {
 				const int mid = first + (last - first) / 2;
@@ -35,15 +35,15 @@ class Solution {
 
 int main(){
 	Solution solution;
-	int a[7] = {1,2,3,4,5,6,6};
-	vector<int> va(a,a+7);
+	int a[8] = {1,1,2,3,4,5,6,6};
+	vector<int> va(a,a+8);
 	for(int i = 0;i < va.size();i++)
 		cout<<a[i]<<" ";
 	cout<<endl;
 	for(vector<int>::iterator it = va.begin();it != va.end();++it)
 		cout<<*it<<" ";
 	cout<<endl;
-	int b = solution.search(va,7);
+	int b = solution.search(va,3);
 	cout<<endl;
 	cout<<b<<endl;
 	return 0;
