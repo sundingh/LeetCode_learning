0a7b5dd490bf43b5df7e71ca17469a3c5226c4b6
sundingh <sundingh@gmail.com>
2016-12-17 13:10:12 +0800

    LeetCode2.1.4

---
+++ Solution2.1.4.cpp
@@ -0,0 +1,51 @@
+/*LeetCode2.1.4
+Follow up for ”Search in Rotated Sorted Array”: What if duplicates are allowed?
+Would this affect the run-time complexity? How and why?
+Write a function to determine if a given target is in the array.
+*/
+
+#include<iostream>
+#include<vector>
+using namespace  std;
+class Solution {
+	public:
+		bool search(const vector<int>& nums, int target) {
+			int first = 0, last = nums.size();
+			while (first != last) {
+				const int mid = first + (last - first) / 2;
+				if (nums[mid] == target)
+					return mid;
+				if (nums[first] < nums[mid]) {
+						if (nums[first] <= target && target < nums[mid])
+							last = mid;
+						else
+							first = mid + 1;
+				} else if (nums[first] > nums[mid]) {
+							if (nums[mid] < target && target <= nums[last-1])
+									first = mid + 1;
+							else
+									last = mid;
+				} else
+						//skip duplicate one
+						first++;
+			}
+			return -1;
+		}
+};
+
+int main(){
+	Solution solution;
+	int a[7] = {1,2,3,4,5,6,6};
+	vector<int> va(a,a+7);
+	for(int i = 0;i < va.size();i++)
+		cout<<a[i]<<" ";
+	cout<<endl;
+	for(vector<int>::iterator it = va.begin();it != va.end();++it)
+		cout<<*it<<" ";
+	cout<<endl;
+	int b = solution.search(va,7);
+	cout<<endl;
+	cout<<b<<endl;
+	return 0;
+	
+}
